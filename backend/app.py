92bf5724dc808f384a72cae7c1c5f7c83016c9aa
Marta <kosiv.marta@gmail.com>
2025-03-31 04:47:39 +0300

    Added home page, made code more readable

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,68 @@
+from datetime import datetime, timezone
 from flask import Flask, render_template
+import re
 
 
 app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend")
+
+
+alarm_data_1 = {
+    "Vinnytska": "notalarm",
+    "Volynska": "alarm",
+    "Dnipropetrovska": "notalarm",
+    "Donetska": "alarm",
+    "Zhytomyrska": "notalarm",
+    "Zakarpatska": "notalarm",
+    "Zaporizka": "notalarm",
+    "Ivano-Frankivska": "notalarm",
+    "Kyivska": "notalarm",
+    "Kirovohradska": "notalarm",
+    "Luhanska": "alarm",
+    "Lvivska": "notalarm",
+    "Mykolaivska": "notalarm",
+    "Odeska": "notalarm",
+    "Poltavska": "notalarm",
+    "Rivnenska": "notalarm",
+    "Sumska": "alarm",
+    "Ternopilska": "notalarm",
+    "Kharkivska": "alarm",
+    "Khersonska": "notalarm",
+    "Khmelnytska": "notalarm",
+    "Cherkaska": "notalarm",
+    "Chernihivska": "notalarm",
+    "Chernivetska": "notalarm",
+    "Kyiv": "notalarm",
+    "Avtonomna Respublika Krym": "notalarm"
+}
+
+alarm_data_2 = {
+    "Vinnytska": "notalarm",
+    "Volynska": "alarm",
+    "Dnipropetrovska": "notalarm",
+    "Donetska": "alarm",
+    "Zhytomyrska": "notalarm",
+    "Zakarpatska": "notalarm",
+    "Zaporizka": "alarm",
+    "Ivano-Frankivska": "notalarm",
+    "Kyivska": "notalarm",
+    "Kirovohradska": "notalarm",
+    "Luhanska": "alarm",
+    "Lvivska": "notalarm",
+    "Mykolaivska": "notalarm",
+    "Odeska": "notalarm",
+    "Poltavska": "notalarm",
+    "Rivnenska": "alarm",
+    "Sumska": "alarm",
+    "Ternopilska": "notalarm",
+    "Kharkivska": "alarm",
+    "Khersonska": "notalarm",
+    "Khmelnytska": "notalarm",
+    "Cherkaska": "notalarm",
+    "Chernihivska": "notalarm",
+    "Chernivetska": "notalarm",
+    "Kyiv": "notalarm",
+    "Avtonomna Respublika Krym": "notalarm"
+}
 
 
 danger_data_1= {
@@ -30,7 +91,7 @@ danger_data_1= {
     "Chernihivska": "notdanger",
     "Chernivetska": "lessdanger ",
     "Kyiv": "lessdanger",
-    "Sevastopilska" : "lessdanger"
+    "Avtonomna Respublika Krym" : "lessdanger"
 }
 danger_data_2= {
     "Vinnytska": "notdanger",
@@ -58,27 +119,31 @@ danger_data_2= {
     "Chernihivska": "ndanger",
     "Chernivetska": "lessdanger ",
     "Kyiv": "lessdanger",
-    "Sevastopilska" : "lessdanger"
+    "Avtonomna Respublika Krym" : "lessdanger"
 }
 
 @app.route("/")
 @app.route("/alarm_map")
 def home():
-    return render_template("alarm_map.html", danger_data=danger_data_1, danger_data_2 = danger_data_2)
+    accure_time = datetime.now(timezone.utc).time()
+    time = re.findall(r'..:..:..', f"{accure_time}")[0]
+    return render_template("alarm_map.html", alarm_data=alarm_data_1, alarm_data_2=alarm_data_2, time=time, \
+                           onpage_map='true', onpage_analytics='false', onpage_help='false', onpage_us='false')
 
 @app.route("/analytics")
 def analytics():
-    return render_template("analytics_map.html", danger_data=danger_data_1, danger_data_2=danger_data_2)
-
-
-@app.route("/about_us")
-def about_us():
-    return render_template("about_us.html")
+    return render_template("analytics_map.html", danger_data=danger_data_1, danger_data_2=danger_data_2, \
+                           onpage_map='false', onpage_analytics='true', onpage_help='false', onpage_us='false')
 
 @app.route("/medical_help")
 def medical_help():
-    return render_template("medical_help.html")
+    return render_template("medical_help.html", \
+                           onpage_map='false', onpage_analytics='false', onpage_help='true', onpage_us='false')
 
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html", \
+                           onpage_map='false', onpage_analytics='false', onpage_help='false', onpage_us='true')
 
 if __name__ == "__main__":
     app.run(debug=True)
