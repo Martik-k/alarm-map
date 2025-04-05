b92dbaeb926a401487743c8cfb545cfc9a39e3d7
Solomia200 <hadiichuk.pn@ucu.edu.ua>
2025-04-06 01:01:31 +0300

    connect data to alarm_map

--- backend/app.py
+++ backend/app.py
@@ -1,69 +1,23 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from flask import Flask, render_template
 import re
-
+import alarm
+import time
+from threading import Thread
 
 app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend")
 
-
-alarm_data_1 = {
-    "Vinnytska": "notalarm",
-    "Volynska": "alarm",
-    "Dnipropetrovska": "notalarm",
-    "Donetska": "alarm",
-    "Zhytomyrska": "notalarm",
-    "Zakarpatska": "notalarm",
-    "Zaporizka": "notalarm",
-    "Ivano-Frankivska": "notalarm",
-    "Kyivska": "notalarm",
-    "Kirovohradska": "notalarm",
-    "Luhanska": "alarm",
-    "Lvivska": "notalarm",
-    "Mykolaivska": "notalarm",
-    "Odeska": "notalarm",
-    "Poltavska": "notalarm",
-    "Rivnenska": "notalarm",
-    "Sumska": "alarm",
-    "Ternopilska": "notalarm",
-    "Kharkivska": "alarm",
-    "Khersonska": "notalarm",
-    "Khmelnytska": "notalarm",
-    "Cherkaska": "notalarm",
-    "Chernihivska": "notalarm",
-    "Chernivetska": "notalarm",
-    "Kyiv": "notalarm",
-    "Avtonomna Respublika Krym": "notalarm"
-}
-
-alarm_data_2 = {
-    "Vinnytska": "notalarm",
-    "Volynska": "alarm",
-    "Dnipropetrovska": "notalarm",
-    "Donetska": "alarm",
-    "Zhytomyrska": "notalarm",
-    "Zakarpatska": "notalarm",
-    "Zaporizka": "alarm",
-    "Ivano-Frankivska": "notalarm",
-    "Kyivska": "notalarm",
-    "Kirovohradska": "notalarm",
-    "Luhanska": "alarm",
-    "Lvivska": "notalarm",
-    "Mykolaivska": "notalarm",
-    "Odeska": "notalarm",
-    "Poltavska": "notalarm",
-    "Rivnenska": "alarm",
-    "Sumska": "alarm",
-    "Ternopilska": "notalarm",
-    "Kharkivska": "alarm",
-    "Khersonska": "notalarm",
-    "Khmelnytska": "notalarm",
-    "Cherkaska": "notalarm",
-    "Chernihivska": "notalarm",
-    "Chernivetska": "notalarm",
-    "Kyiv": "notalarm",
-    "Avtonomna Respublika Krym": "notalarm"
-}
-
+# Ініціалізація глобальної змінної
+alarm_data_1 = {}
+current_time  = "00:00:00"
+def get_data():
+    global alarm_data_1
+    global current_time 
+    while True:
+        alarm_data_1 = alarm.get_active_alerts()
+        accure_time = datetime.now().time()
+        current_time = re.findall(r'..:..:..', f"{accure_time}")[0]
+        time.sleep(30)
 
 danger_data_1= {
     "Vinnytska": "notdanger",
@@ -124,18 +78,17 @@ danger_data_2= {
     "Avtonomna Respublika Krym" : "lessdanger"
 }
 
-
 @app.route("/")
 @app.route("/alarm_map")
 def home():
-    accure_time = datetime.now().time()
-    time = re.findall(r'..:..:..', f"{accure_time}")[0]
-    return render_template("alarm_map.html", alarm_data=alarm_data_1, alarm_data_2=alarm_data_2, time=time, \
+    # accure_time = datetime.now().time()
+    # current_time = re.findall(r'..:..:..', f"{accure_time}")[0]
+    return render_template("alarm_map.html", alarm_data=alarm_data_1, time=current_time, \
                            onpage_map='true', onpage_analytics='false', onpage_help='false', onpage_us='false')
 
 @app.route("/analytics")
 def analytics():
-    return render_template("analytics_map.html", danger_data=danger_data_1,danger_data_1=danger_data_1, danger_data_2=danger_data_2, \
+    return render_template("analytics_map.html", danger_data=danger_data_1, danger_data_1=danger_data_1, danger_data_2=danger_data_2, \
                            onpage_map='false', onpage_analytics='true', onpage_help='false', onpage_us='false')
 
 @app.route("/medical_help")
@@ -147,6 +100,3 @@ def medical_help():
 def about_us():
     return render_template("about_us.html", \
                            onpage_map='false', onpage_analytics='false', onpage_help='false', onpage_us='true')
-
-if __name__ == "__main__":
-    app.run(debug=True)
