cf66c8c0682aee38319ed80d682fd635f9455b22
Solomia200 <hadiichuk.pn@ucu.edu.ua>
2025-04-06 01:03:25 +0300

    Add another thread

--- backend/run_waitress.py
+++ backend/run_waitress.py
@@ -1,5 +1,10 @@
 from waitress import serve
-from app import app
+from app import app, get_data
+from threading import Thread
+
+
+data_thread = Thread(target=get_data)
+data_thread.start()
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=8000)
